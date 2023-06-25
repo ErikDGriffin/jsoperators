9fe50d5eefeff619dbf01ff7a5dad55f2405331a
erik Griffin <griffin.erikd@gmail.com>
2023-06-25 12:16:42 -0400

    added exercise

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n");
+
+  // Exercise 1
+let a = 20;
+let b = 4;
+let sum = a + b;
+let minus = a - b;
+let multiply= a * b;
+let divide= a / b;
+console.log(a, b, minus, multiply, divide);
+
+// Exercise 2
+let num = 1111
+let str = "11eleven";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo11";
+let lastName = "Baggins";
+console.log(num + str, num = str2, num + isPresent, firstName + num, isPresent + str, firstName = lastName);
+
+
+// Exercise 3
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false
+console.log( eleven ==str4 && val >=str3);
+console.log(val >=str3)
+console.log("experiment", (!true || true));
+console.log(o== false);
+console.log(0 === false);
+console.log(0 != false)
+/*
+// What is the value of: val == str3? true
+//What is the value of: val === str3? false
+// What is the value of: !isPresent2? true
+//What is the value of: (“eleven” == str4 && val >= str3)? false
+//What is the value of: (!isPresent2 || isPresent2)? true
+//What is the value of: 0 == false? true
+//What is the value of: 0 === false? false
+//What is the value of: 0 != false? false
+//What is the value of 0 !== false? true
